f303a6dc857876e68d1926d85aaecee21e4d75c0
chanjeong park <ckswjd9595@gmail.com>
2025-01-20 21:28:12 +0900

    백준 1차원 배열 공부

--- Backjoon/index.js
+++ Backjoon/index.js
@@ -1,18 +1,10 @@
 let input = require('fs')
   .readFileSync('C:/Users/ckswj/Desktop/Zerobase/Backjoon/example.txt', 'utf-8')
-  .toString().trim()
-  .split('\n')
-// let input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
+  .toString()
+  .trim()
+  .split('\n');
+// let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
 
-const filtered = input.map(el => Number(el.trim()));
-
-let array = [];
-
-for(let i = 0; i < filtered.length; i++){
-  let a = filtered[i] % 42;
-  if(!array.includes(a)){
-    array.push(a)
-  }
+for (let i = 0; i < input.length; i++) {
+  console.log(input[i]);
 }
-console.log(array.length);
-
