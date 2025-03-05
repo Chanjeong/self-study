8da6a691d34e660c118914151a32442fb5c31c93
chanjeong park <ckswjd9595@gmail.com>
2025-03-05 16:53:27 +0900

    코테 연습

--- Backjoon/index.js
+++ Backjoon/index.js
@@ -5,24 +5,6 @@ let input = require('fs')
   .split('\n');
 // let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
 
-let [A, B] = input.map(Number);
-
-function isPrime(n) {
-  if (n < 2) return false; // 1은 소수가 아님
-  for (let i = 2; i * i <= n; i++) {
-    if (n % i === 0) return false;
-  }
-  return true;
-}
-
-let res = [];
-for (let i = A; i <= B; i++) {
-  if (isPrime(i)) res.push(i);
-}
-
-if (res.length === 0) {
-  console.log(-1);
-} else {
-  console.log(res.reduce((acc, cum) => acc + cum, 0));
-  console.log(res[0]);
-}
+const [x, y, w, h] = input[0].split(' ').map(Number);
+console.log(x, y, w, h);
+console.log(Math.min(w - x, x, h - y, y));
