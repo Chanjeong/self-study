551fdcbcf1445b8b286247ad39abde9d626f608e
chanjeong park <ckswjd9595@gmail.com>
2025-03-06 15:53:20 +0900

    코테

--- Backjoon/index.js
+++ Backjoon/index.js
@@ -5,6 +5,12 @@ let input = require('fs')
   .split('\n');
 // let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
 
-const [x, y, w, h] = input[0].split(' ').map(Number);
-console.log(x, y, w, h);
-console.log(Math.min(w - x, x, h - y, y));
+const [a, b, c] = input.map(element => +element.trim());
+
+if (a + b + c !== 180) console.log('Error');
+else {
+  if (a === 60 && b === 60 && c === 60) console.log('Equilateral');
+  else if ((a === b && a !== c) || (a === c && b !== c) || (b === c && a !== c))
+    console.log('Isosceles');
+  else console.log('Scalene');
+}
