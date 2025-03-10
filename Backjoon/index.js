f04aac9842bcf51fcf5777e9f956317ff1b57a7d
chanjeong park <ckswjd9595@gmail.com>
2025-03-10 14:52:08 +0900

    브루트포스 코테

--- Backjoon/index.js
+++ Backjoon/index.js
@@ -5,8 +5,10 @@ let input = require('fs')
   .split('\n');
 // let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
 
-const [a, b] = input[0].split(' ').map(Number);
-const c = +input[1];
-const n0 = +input[2];
-
-b <= (c - a) * n0 && c >= a ? console.log(1) : console.log(0);
+let num = 0;
+while (num++ !== +input[0]) {
+  const [...element] = (num + '').split('').map(Number);
+  const total = [...element].reduce((acc, cum) => acc + cum, 0);
+  if (total + num === +input[0]) return console.log(num);
+}
+return console.log(0);
