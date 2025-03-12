aaca06c069e91d0a1b444046cd18fea802ff71d6
chanjeong park <ckswjd9595@gmail.com>
2025-03-12 20:17:36 +0900

    브루트포스 코테

--- Backjoon/index.js
+++ Backjoon/index.js
@@ -5,9 +5,7 @@ let input = require('fs')
   .split('\n');
 // let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
 
-let [a, b, c, d, e, f] = input[0].split(' ').map(Number);
-
-const x = (c * e - b * f) / (a * e - b * d);
-const y = (a * f - c * d) / (a * e - b * d);
-
-console.log(parseInt(x), parseInt(y));
+const [num, ...boardLines] = [...input].map(el => el.trim());
+const [a, b] = num.split(' ').map(Number);
+const board = boardLines.map(line => line.split(''));
+console.log(board);
