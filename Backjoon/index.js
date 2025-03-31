003fbb00bdeb835a78d618dbe7bd6240c89f97a2
chanjeong park <ckswjd9595@gmail.com>
2025-03-31 16:24:58 +0900

    정렬 코테

--- Backjoon/index.js
+++ Backjoon/index.js
@@ -7,10 +7,10 @@ let input = require('fs')
 
 let arr = [];
 for (let i = 1; i < input.length; i++) {
-  arr.push(+input[i].trim());
+  arr.push(input[i].trim().split(' ').map(Number));
 }
+arr.sort(([a, b], [c, d]) => a - c || b - d);
 
-arr.sort((a, b) => a - b);
 for (let i = 0; i < arr.length; i++) {
-  console.log(arr[i]);
+  console.log(arr[i].join(' '));
 }
