9bced7516340fbfc99331c8b2d57204b8cb5aad7
chanjeong park <ckswjd9595@gmail.com>
2025-04-14 16:57:36 +0900

    스택, 데크큐

--- Backjoon/index.js
+++ Backjoon/index.js
@@ -5,23 +5,85 @@ let input = require('fs')
   .split('\n');
 // let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
 
-let stack = [];
-let line = input[1].split(' ').map(Number);
-
-let order = 1;
-
-while (line.length > 0 || stack.length > 0) {
-  if (line[0] === order) {
-    line.shift();
-    order += 1;
-  } else if (stack[stack.length - 1] === order) {
-    stack.pop();
-    order += 1;
-  } else if (line.length > 0) {
-    stack.push(line.shift());
-  } else {
-    console.log('Sad');
-    return;
+class Node {
+  constructor(item) {
+    this.item = item;
+    this.next = null;
   }
 }
-console.log('Nice');
+
+class Queue {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.size = 0;
+  }
+
+  push(item) {
+    const node = new Node(item);
+    if (!this.head) {
+      this.head = node;
+      this.head.next = this.tail;
+    } else {
+      this.tail.next = node;
+    }
+
+    this.tail = node;
+    this.size += 1;
+  }
+
+  getSize() {
+    return this.size;
+  }
+
+  empty() {
+    return this.size === 0 ? 1 : 0;
+  }
+
+  pop() {
+    if (this.size > 2) {
+      const item = this.head.item;
+      const newHead = this.head.next;
+      this.head = newHead;
+      this.size -= 1;
+      return item;
+    } else if (this.size === 2) {
+      const item = this.head.item;
+      const newHead = this.head.next;
+      this.head = newHead;
+      this.tail = newHead;
+      this.size -= 1;
+      return item;
+    } else if (this.size === 1) {
+      const item = this.head.item;
+      this.head = null;
+      this.tail = null;
+      this.size -= 1;
+      return item;
+    } else {
+      return -1;
+    }
+  }
+  front() {
+    return this.head ? this.head.item : -1;
+  }
+  back() {
+    return this.tail ? this.tail.item : -1;
+  }
+}
+
+function solution(input) {
+  const queue = new Queue();
+
+  for (let i = 1; i <= +input[0]; i++) {
+    queue.push(i);
+  }
+  while (queue.size !== 1) {
+    queue.pop();
+    queue.push(queue.pop());
+  }
+  return queue.head.item;
+}
+
+const answer = solution(input);
+console.log(answer);
