11cedaf376e2426d7f9a72cb6c114cb7470e1385
chanjeong park <ckswjd9595@gmail.com>
2025-04-15 16:42:44 +0900

    데크

--- Backjoon/index.js
+++ Backjoon/index.js
@@ -6,84 +6,19 @@ let input = require('fs')
 // let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
 
 class Node {
-  constructor(item) {
-    this.item = item;
+  constructor(value) {
+    this.value = value;
+    this.prev = null;
     this.next = null;
   }
 }
 
-class Queue {
+class Deque {
   constructor() {
     this.head = null;
     this.tail = null;
-    this.size = 0;
+    this.length = 0;
   }
 
-  push(item) {
-    const node = new Node(item);
-    if (!this.head) {
-      this.head = node;
-      this.head.next = this.tail;
-    } else {
-      this.tail.next = node;
-    }
-
-    this.tail = node;
-    this.size += 1;
-  }
-
-  getSize() {
-    return this.size;
-  }
-
-  empty() {
-    return this.size === 0 ? 1 : 0;
-  }
-
-  pop() {
-    if (this.size > 2) {
-      const item = this.head.item;
-      const newHead = this.head.next;
-      this.head = newHead;
-      this.size -= 1;
-      return item;
-    } else if (this.size === 2) {
-      const item = this.head.item;
-      const newHead = this.head.next;
-      this.head = newHead;
-      this.tail = newHead;
-      this.size -= 1;
-      return item;
-    } else if (this.size === 1) {
-      const item = this.head.item;
-      this.head = null;
-      this.tail = null;
-      this.size -= 1;
-      return item;
-    } else {
-      return -1;
-    }
-  }
-  front() {
-    return this.head ? this.head.item : -1;
-  }
-  back() {
-    return this.tail ? this.tail.item : -1;
-  }
+  push_front(value) {}
 }
-
-function solution(input) {
-  const queue = new Queue();
-
-  for (let i = 1; i <= +input[0]; i++) {
-    queue.push(i);
-  }
-  while (queue.size !== 1) {
-    queue.pop();
-    queue.push(queue.pop());
-  }
-  return queue.head.item;
-}
-
-const answer = solution(input);
-console.log(answer);
