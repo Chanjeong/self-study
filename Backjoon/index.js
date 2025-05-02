edd31a2dfa1b6d233984b5d23add371ed1311448
chanjeong park <ckswjd9595@gmail.com>
2025-05-02 16:01:45 +0900

    백트래킹 다시 처음부터

--- Backjoon/index.js
+++ Backjoon/index.js
@@ -4,33 +4,23 @@ let input = require('fs')
   .trim()
   .split('\n');
 // let input = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
+const [N, K] = input[0].trim().split(' ').map(Number);
 
-const [N, A, operator] = input.map(v => v.split(' ').map(Number));
-
-const calculator = [
-  (a, b) => a + b,
-  (a, b) => a - b,
-  (a, b) => a * b,
-  (a, b) => ~~(a / b)
-];
-
-let max = -Infinity;
-let min = Infinity;
-
-const dfs = (count = 0, result = A[0]) => {
-  if (count === N - 1) {
-    max = Math.max(result, max);
-    min = Math.min(result, min);
+let result = [];
+function dfs(depth, ones) {
+  if (depth === N) {
+    console.log(result.join(''));
     return;
   }
-  for (let i = 0; i < 4; i++) {
-    if (!operator[i]) continue;
-    operator[i]--;
-    dfs(count + 1, calculator[i](result, A[count + 1]));
-    operator[i]++;
-  }
-};
 
-dfs();
-console.log(max);
-console.log(min);
+  result.push('0');
+  dfs(depth + 1, ones);
+  result.pop();
+  if (ones < K) {
+    result.push('1');
+    dfs(depth + 1, ones + 1);
+    result.pop();
+  }
+}
+
+dfs(0, 0);
