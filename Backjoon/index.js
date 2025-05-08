80ea8420f09c59bf03d801c6fbcc224c80fd8b69
chanjeong park <ckswjd9595@gmail.com>
2025-05-08 19:37:27 +0900

    실버1문제

--- Backjoon/index.js
+++ Backjoon/index.js
@@ -4,3 +4,17 @@ let input = require('fs')
   .trim()
   .split('\n');
 // let input = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
+
+let col = +input[0];
+let row = 1;
+while (col - row > 0) {
+  col = col - row;
+  row += 1;
+}
+
+//col은 그 대각선 안에서의 위치로 바뀌게 됨
+if (row % 2 === 1) {
+  console.log(`${row + 1 - col}/${col}`);
+} else {
+  console.log(`${col}/${row + 1 - col}`);
+}
