45764fab4c9b33c835f0636c3f3c083e47aa87c6
chanjeong park <ckswjd9595@gmail.com>
2025-05-20 17:12:14 +0900

    1343번-폴리노미오

--- Backjoon/index.js
+++ Backjoon/index.js
@@ -4,45 +4,24 @@ let input = require('fs')
   .trim()
   .split('\n');
 // let input = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
-const [monthStr, dayStr, year, timeStr] = input[0].split(' ');
-const [hour, minute] = timeStr.split(':').map(Number);
-const day = parseInt(dayStr.replace(',', ''), 10);
-console.log(hour, minute);
-// 월 이름 → 숫자 매핑
-const monthMap = {
-  January: 1,
-  February: 2,
-  March: 3,
-  April: 4,
-  May: 5,
-  June: 6,
-  July: 7,
-  August: 8,
-  September: 9,
-  October: 10,
-  November: 11,
-  December: 12
-};
 
-const month = monthMap[monthStr];
+const board = input[0].split('.');
+let result = '';
+for (let i = 0; i < board.length; i++) {
+  const segment = board[i];
+  const len = segment.length;
 
-const isLeap = y => (y % 4 === 0 && y % 100 !== 0) || y % 400 === 0;
-const totalDays = isLeap(+year) ? 366 : 365;
+  if (len % 2 !== 0) {
+    console.log(-1);
+    return;
+  }
 
-const inputMonth = String(month).padStart(2, '0');
-const inputDay = String(day).padStart(2, '0');
-const inputHour = String(hour).padStart(2, '0');
-const inputMinute = String(minute).padStart(2, '0');
+  if (len === 0) result += '.';
 
-const now = new Date(
-  `${year}-${inputMonth}-${inputDay}T${inputHour}:${inputMinute}:00`
-);
-const startOfYear = new Date(`${year}-01-01T00:00:00`);
+  const aCount = Math.floor(len / 4);
+  const bCount = (len % 4) / 2;
 
-// 밀리초 차이 → 초 → 비율
-const passedSeconds = (now - startOfYear) / 1000;
-const totalSeconds = totalDays * 24 * 60 * 60;
-
-const percentage = (passedSeconds / totalSeconds) * 100;
-
-console.log(percentage.toPrecision(17));
+  result += 'AAAA'.repeat(aCount) + 'BB'.repeat(bCount);
+  if (len !== 0) result += '.';
+}
+console.log(result);
