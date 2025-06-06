2014d054a69fbdd23b0ab676768203d37f916170
chanjeong park <ckswjd9595@gmail.com>
2025-06-06 16:56:03 +0900

    실버2

--- Backjoon/index.js
+++ Backjoon/index.js
@@ -5,36 +5,8 @@ let input = require('fs')
   .split('\n');
 // let input = require("fs").readFileSync("/dev/stdin").toString().trim().split('\n');
 
-let index = 0;
-let groupNum = 1;
-let group = [];
-let scoreGroup = [];
-let count = 0;
-while (true) {
-  let n = parseInt(input[index]);
+const n = +input[0];
+const group = input[1].trim().split(' ').map(Number);
+const arr = new Array(n);
 
-  if (n === 0) break;
-
-  for (let i = index + 1; i < index + 1 + n; i++) {
-    const [student, ...score] = input[i].trim().split(' ');
-    group.push(student);
-    scoreGroup.push(score);
-  }
-  console.log(`Group ${groupNum}`);
-  for (let i = 0; i < scoreGroup.length; i++) {
-    for (let j = 0; j < scoreGroup[i].length; j++) {
-      if (scoreGroup[i][j] === 'N') {
-        const target = (i - j - 1 + n) % n;
-        console.log(`${group[target]} was nasty about ${group[i]}`);
-        count += 1;
-      }
-    }
-  }
-  if (count === 0) console.log(`Nobody was nasty about Debby`);
-  console.log('\r');
-  count = 0;
-  groupNum += 1;
-  group = [];
-  scoreGroup = [];
-  index = index + n + 1;
-}
+for (let i = 0; i < group.length; i++) {}
